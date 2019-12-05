c3a6ad505eebcb47d15ef1dd99be70f90f04be16
kaja-h <karolinaa.haran@gmail.com>
2019-12-05 22:59:51 +0100

    async

--- users/companies.js
+++ users/companies.js
@@ -1,38 +1,65 @@
-fetch("http://localhost:3000/users")
-    .then(resp => {
-        console.log("Przykład 1:");
-        console.log(resp);
-    })
+// fetch("http://localhost:3000/users")
+//     .then(resp => {
+//         console.log("Przykład 1:");
+//         console.log(resp);
+//     })
+//
+// fetch("http://localhost:3000/companies")
+//     .then(resp => {
+//         console.log("Przykład 1:");
+//         console.log(resp);
+//     })
+//
+// fetch("http://localhost:3000/users")
+//     .then(resp => resp.json())
+//     .then(resp => {
+//         console.log("Przykład 2:");
+//         console.log(resp);
+//     })
+//
+// fetch("http://localhost:3000/companies")
+//     .then(resp => resp.json())
+//     .then(resp => {
+//         console.log("Przykład 2:");
+//         console.log(resp);
+//     })
 
-fetch("http://localhost:3000/companies")
-    .then(resp => {
-        console.log("Przykład 1:");
-        console.log(resp);
-    })
+// fetch("http://localhost:3000/users")
+//     .then(resp => resp.json())
+//     .then(resp => {
+//         console.log(resp);
+//
+//         resp.forEach(user => {
+//             console.groupCollapsed(`Użytkownik ${user.uri}`)
+//             console.log(`Nazwa: ${user.name}`);
+//             console.log(`Email: ${user.email}`);
+//             console.log(`Firma: ${user.uris.company}`)
+//             console.groupEnd();
+//         })
+//     })
 
-fetch("http://localhost:3000/users")
-    .then(resp => resp.json())
-    .then(resp => {
-        console.log("Przykład 2:");
-        console.log(resp);
-    })
+async function fetchData() {
+    try {
+        const response = await Promise.all([
+            fetch("http://localhost:3000/users")
+                .then(response => response.json()),
+            fetch("http://localhost:3000/companies")
+                .then(response => response.json())
+        ]);
+        console.log(response);
 
-fetch("http://localhost:3000/companies")
-    .then(resp => resp.json())
-    .then(resp => {
-        console.log("Przykład 2:");
-        console.log(resp);
-    })
-
-fetch("http://localhost:3000/users")
-    .then(resp => resp.json())
-    .then(resp => {
-        console.log(resp);
-
-        resp.forEach(user => {
+        const table = document.getElementById('list');
+        response.forEach(user => {
             console.groupCollapsed(`Użytkownik ${user.uri}`)
             console.log(`Nazwa: ${user.name}`);
             console.log(`Email: ${user.email}`);
+            // console.log(`Firma: ${user.uris.company}`)
             console.groupEnd();
-        })
-    })
+        });
+      } catch (err) {
+        console.log(err);
+      }
+
+}
+fetchData()
+
