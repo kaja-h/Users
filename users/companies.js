548cc9c7b284307243923170c9a5ff59457b748e
kaja-h <karolinaa.haran@gmail.com>
2019-12-06 22:57:49 +0100

    table

--- users/companies.js
+++ users/companies.js
@@ -40,26 +40,38 @@
 
 async function fetchData() {
     try {
-        const response = await Promise.all([
-            fetch("http://localhost:3000/users")
-                .then(response => response.json()),
-            fetch("http://localhost:3000/companies")
-                .then(response => response.json())
-        ]);
-        console.log(response);
+        const response = await fetch("http://localhost:3000/users").then(response => response.json());
+        const resp = await fetch("http://localhost:3000/companies").then(resp => resp.json());
 
         const table = document.getElementById('list');
-        response.forEach(user => {
-            console.groupCollapsed(`Użytkownik ${user.uri}`)
-            console.log(`Nazwa: ${user.name}`);
-            console.log(`Email: ${user.email}`);
-            // console.log(`Firma: ${user.uris.company}`)
-            console.groupEnd();
-        });
+        const rows = resp.map(x => Object.assign(x, response.find(y => y.uris.company == x.uri)));
+        rows.forEach(user => {
+            const tr = document.createElement('tr');
+            tr.innerHTML = '<td>' + user.uris.company + '</td>' +
+            '<td>' + user.name + '</td>';
+            table.appendChild(tr);
+        })
+
+        // resp.forEach(company => {
+        //     const tr = document.createElement('tr');
+        //     let td = document.getElementById('company');
+        //     tr.innerHTML = '<tr><td>' + company.name + '</td></tr>';
+        //     tr.appendChild(td);
+        //     table.appendChild(tr);
+        // });
+        // response.forEach(user => {
+        //     const tr = document.createElement('tr');
+        //     let td = document.getElementById('user');
+        //     tr.innerHTML = '<tr><td>' + user.name + '</td></tr>';
+        //     tr.appendChild(td);
+        //     table.appendChild(tr);
+        // });
+        console.log(response, resp, rows);
+        return Promise.all([response, resp]);
+
       } catch (err) {
         console.log(err);
       }
-
 }
 fetchData()
 
