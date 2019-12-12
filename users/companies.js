635e8334add3afba12cee96cb9652c38dce4105a
kaja-h <karolinaa.haran@gmail.com>
2019-12-12 17:15:23 +0100

    added then at the end

--- users/companies.js
+++ users/companies.js
@@ -1,13 +1,12 @@
 async function fetchData() {
     try {
-        const response = await fetch("http://localhost:3000/users").then(response => response.json());
         const resp = await fetch("http://localhost:3000/companies").then(resp => resp.json());
-
+        const response = await fetch("http://localhost:3000/users").then(response => response.json());
         const list = resp.map(function(company) {
             return {
                 name: company.name,
                 users: response.filter(function (user) {
-                    return user.uris.company === company.uri;
+                    return company.uri === user.uris.company;
                 }).map(function (user) {
                     return {
                         id: user.name
@@ -15,21 +14,44 @@ async function fetchData() {
                 })
             }
         });
+        const data = Promise.all([resp,response, list]);
         const table = document.getElementById('table');
+
+        // for (let index = 0; index < list.length; index++) {
+        //        const id = list.users[index];
+        //        const tr = document.createElement('tr');
+        //        tr.innerHTML = '<td>' + list.name + '</td>' +
+        //        '<td>' + id + '</td>';
+        //        table.appendChild(tr);
+        //     }
         list.forEach(list => {
             const tr = document.createElement('tr');
             tr.innerHTML = '<td>' + list.name + '</td>' +
-            (list.users).forEach(index => {
-                tr.innerHTML = '<td>' + list.users[index] + '</td>';
-            });
+                '<td>' + list.users.id + '</td>';
             table.appendChild(tr);
         });
-        console.log(response, resp, list);
-        return Promise.all([response, resp]);
+        // for (let i = 1; i < list.length; i++) {
+        //     const tr = document.createElement('tr');
+        //     tr.innerHTML = '<td>' + list.users[i].id + '</td>';
+        //     table.appendChild(tr);
+        // }
+
+        return data;
 
       } catch (err) {
         console.log(err);
       }
 }
+
 fetchData()
+    .then(data => console.log(data));
+
+
+
+
+
+
+
+
+
 
