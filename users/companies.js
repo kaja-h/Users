f0427e5fb887033ff82a2953cd9c325c98906633
kaja-h <karolinaa.haran@gmail.com>
2019-12-16 23:00:40 +0100

    2 async functions

--- users/companies.js
+++ users/companies.js
@@ -1,57 +1,51 @@
-async function fetchData() {
+async function companies() {
     try {
         const resp = await fetch("http://localhost:3000/companies").then(resp => resp.json());
-        const response = await fetch("http://localhost:3000/users").then(response => response.json());
-        const list = resp.map(function(company) {
-            return {
-                name: company.name,
-                users: response.filter(function (user) {
-                    return company.uri === user.uris.company;
-                }).map(function (user) {
-                    return {
-                        id: user.name
-                    }
-                })
-            }
-        });
-        const data = Promise.all([resp,response, list]);
-        const table = document.getElementById('table');
-
-        // for (let index = 0; index < list.length; index++) {
-        //        const id = list.users[index];
-        //        const tr = document.createElement('tr');
-        //        tr.innerHTML = '<td>' + list.name + '</td>' +
-        //        '<td>' + id + '</td>';
-        //        table.appendChild(tr);
-        //     }
-        list.forEach(list => {
-            const tr = document.createElement('tr');
-            tr.innerHTML = '<td>' + list.name + '</td>' +
-                '<td>' + list.users.id + '</td>';
-            table.appendChild(tr);
-        });
-        // for (let i = 1; i < list.length; i++) {
-        //     const tr = document.createElement('tr');
-        //     tr.innerHTML = '<td>' + list.users[i].id + '</td>';
-        //     table.appendChild(tr);
-        // }
-
-        return data;
+        console.log(resp);
 
       } catch (err) {
         console.log(err);
       }
-}
+};
 
-fetchData()
-    .then(data => console.log(data));
+async function users() {
+    try {
+        const response = await fetch("http://localhost:3000/users").then(response => response.json());
+        console.log(response);
 
+    } catch (err) {
+        console.log(err);
+    }
+};
 
+function run()  {
+        companies()
+            .then(users());
+        const list = .map(function (company) {
+                return {
+                    name: company.name,
+                    user: .filter(function (user) {
+                        return company.uri === user.uris.company;
+                    }).map(function (user) {
+                        return {
+                            id: user.name
+                        }
+                    })
+                }
+        })
+};
+run()
 
+// const table = document.getElementById('table');
+// list.forEach(list => {
+//     const tr = document.createElement('tr');
+//     tr.innerHTML = '<td>' + list.users.id + '</td>';
+//     table.appendChild(tr);
+// })
 
-
-
-
-
-
-
+// list.forEach(list => {
+//     const tr = document.createElement('tr');
+//     tr.innerHTML = '<td>' + list.name + '</td>' +
+//         '<td>' + list.users + '</td>';
+//     table.appendChild(tr);
+// });
